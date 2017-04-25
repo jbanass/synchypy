90fa9779c4bc8a606dec15778eaefbd876e131af
Joseph Banass <joe@morsedata.com>
2017-04-24 22:09:13 -0500

    conforming to PEP8 standards.

--- syncy.py
+++ syncy.py
@@ -1,33 +1,38 @@
-import os, argparse, ftplib, sys
+'''Syncy.py synchronizes a local directory with a remote ftp server'''
+import os
+import argparse
+import ftplib
+import sys
 
-def GetCredentials(keyFile):
+def get_credentials(key_file):
+    '''Obtains credentials from key_file'''
     creds = []
 
-    if os.path.exists(keyFile) and os.path.isfile(keyFile):
-        credFile = open(keyFile, 'r')
-        creds = credFile.readline()
-        credFile.close()
+    if os.path.exists(key_file) and os.path.isfile(key_file):
+        credential_file = open(key_file, 'r')
+        creds = credential_file.readline()
+        credential_file.close()
     else:
-        raise FileNotFoundError("Key File {} not found.".format(keyFile))
-        quit(-1)
+        raise FileNotFoundError("Key File {} not found.".format(key_file))
 
     creds = creds.split(',')
-    
     return creds[0], creds[1], creds[2]
 
-def WipeFTP(connection, destinationDirectory):
-    '''Delete all files within destinationDirectory'''
+def wipe_ftp(connection, destination_directory):
+    '''Delete all files within destination_directory'''
     return
 
-def PopulateFTP(connection, distributionDirectory, destinationDirectory):
+def populate_ftp(connection, distribution_directory, destination_directory):
     '''Populate a FTP destination with files from distribution'''
     return
 
-def Deploy(distributionDirectory, destinationDirectory, keyFile):
+def deploy(distribution_directory, destination_directory, key_file):
+    '''Deploys files from distribution_directory to destination_directory \
+    with credentials supplied by key_file'''
     user = ''
     key = ''
     addr = ''
-    user, key, addr = GetCredentials(keyFile)
+    user, key, addr = get_credentials(key_file)
 
     connection = None
     try:
@@ -35,26 +40,27 @@ def Deploy(distributionDirectory, destinationDirectory, keyFile):
         connection = ftplib.FTP(addr, user, key)
         print("Successfully connected.")
 
-        WipeFTP(connection, destinationDirectory)
-        PopulateFTP(connection, distributionDirectory, destinationDirectory)
+        wipe_ftp(connection, destination_directory)
+        populate_ftp(connection, distribution_directory, destination_directory)
 
         connection.close()
     except Exception:
         print("Unexpected error:", sys.exc_info())
 
-def ListFiles(distributionDirectory):
-    for dirpath, dirnames, filenames in os.walk(distributionDirectory):
+def list_files(distribution_directory):
+    '''Lists files in distribution_directory'''
+    for dirpath, dirnames, filenames in os.walk(distribution_directory):
         for filename in filenames:
             print(os.path.join(dirpath, filename))
 
 
-parser = argparse.ArgumentParser(description="Deploy files and folders to a remote webserver.")
+PARSER = argparse.ArgumentParser(description="Deploy files and folders to a remote webserver.")
 
-parser.add_argument('s', help="Source directory", type=str)
-parser.add_argument('t', help="Target directory", type=str)
-parser.add_argument('b', help="Backup directory", type=str)
-parser.add_argument('keyfile', help="Keyfile directory", type=str)
+PARSER.add_argument('s', help="Source directory", type=str)
+PARSER.add_argument('t', help="Target directory", type=str)
+PARSER.add_argument('b', help="Backup directory", type=str)
+PARSER.add_argument('keyfile', help="Keyfile directory", type=str)
 
-args = parser.parse_args()
+ARGS = PARSER.parse_args()
 
-Deploy(args.s, args.t, args.b, args.keyfile)
+deploy(ARGS.s, ARGS.t, ARGS.keyfile)
