2f212325e2da892b3a176d0dccba4ffaad50d73f
Joseph Banass <joe@morsedata.com>
2017-04-25 21:52:19 -0500

    remote folder and file creation working. Next step is backing up then deletion of remote directory before push.

--- syncy.py
+++ syncy.py
@@ -18,15 +18,51 @@ def get_credentials(key_file):
     creds = creds.split(',')
     return creds[0], creds[1], creds[2]
 
-def wipe_ftp(connection, destination_directory):
+def wipe_ftp(connection):
     '''Delete all files within destination_directory'''
     return
 
-def populate_ftp(connection, distribution_directory, destination_directory):
-    '''Populate a FTP destination with files from distribution'''
-    return
+def translate_local_to_ftp(files_dict: dict, distribution_directory: str):
+    '''Performs directory translation between a nested directory locally to \
+     a normalized directory remotely'''
+    return_dict = {}
 
-def deploy(distribution_directory, destination_directory, key_file):
+    for key in files_dict:
+        if key == distribution_directory:
+            return_dict['/'] = files_dict[key]
+        else:
+            translated_key = '/' + key.replace('\\', '/')[len(distribution_directory):len(key)] + '/'
+            return_dict[translated_key] = files_dict[key]
+
+    return return_dict
+
+def populate_ftp(connection, distribution_directory):
+    '''Populate a FTP destination with files from distribution'''
+    files_dict = {}
+
+    for root, dirs, files in os.walk(distribution_directory):
+        files_dict[root] = (dirs, files)
+
+    ftp_dict = translate_local_to_ftp(files_dict, distribution_directory)
+
+    #make folders
+    for folders in ftp_dict:
+        for folder in ftp_dict[folders][0]:
+            print("Making folder: {}{}".format(folders, folder))
+            connection.mkd(folders + folder)
+    #transfer files
+    for folders in ftp_dict:
+        for files in ftp_dict[folders][1]:
+            normalized_path = distribution_directory +  \
+                folders[1:len(folders)].replace("/", "\\") + files
+            normalized_ftp_path = folders + files
+            print("Transferring file from {} to {}".format(normalized_path, normalized_ftp_path))
+            myfile = open(normalized_path, 'rb')
+            connection.storbinary("STOR {}".format(normalized_ftp_path), myfile)
+            myfile.close()
+
+
+def deploy(distribution_directory, key_file):
     '''Deploys files from distribution_directory to destination_directory \
     with credentials supplied by key_file'''
     user = ''
@@ -40,8 +76,8 @@ def deploy(distribution_directory, destination_directory, key_file):
         connection = ftplib.FTP(addr, user, key)
         print("Successfully connected.")
 
-        wipe_ftp(connection, destination_directory)
-        populate_ftp(connection, distribution_directory, destination_directory)
+        wipe_ftp(connection)
+        populate_ftp(connection, distribution_directory)
 
         connection.close()
     except Exception:
@@ -57,10 +93,8 @@ def list_files(distribution_directory):
 PARSER = argparse.ArgumentParser(description="Deploy files and folders to a remote webserver.")
 
 PARSER.add_argument('s', help="Source directory", type=str)
-PARSER.add_argument('t', help="Target directory", type=str)
-PARSER.add_argument('b', help="Backup directory", type=str)
 PARSER.add_argument('keyfile', help="Keyfile directory", type=str)
 
 ARGS = PARSER.parse_args()
 
-deploy(ARGS.s, ARGS.t, ARGS.keyfile)
+deploy(ARGS.s, ARGS.keyfile)
